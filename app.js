aa8d56ad6f1d8c3f0a134f4257b6509890123224
Kyle Aleman <kylealeman18@gmail.com>
2023-10-29 00:49:31 +0000

    add stuff for the lab

---
+++ app.js
@@ -0,0 +1,13 @@
+let askName = prompt ('What is your name?');
+let askFood = prompt ("Let's get to know each other! What is your favorite food?");
+let askHobby = prompt ('Favorite hobbie?');
+let askLast = prompt (`What is your favorite movie?`)
+console.log(askName);
+console.log(askFood);
+console.log(askHobby);
+console.log(askLast);
+
+
+
+
+alert(`Hello ${askName}, welcome to my website. Here is some things I learned about you. Your favorite food is ${askFood} and your favorite hobby is ${askHobby}. Something about me is I hate watching ${askLast}. :)`);
